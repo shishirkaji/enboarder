4e09613f18c9762f57453811e4379eb69c9500d8
Binam Rajbhandari <shishirkaji@gmail.com>
2020-10-25 02:12:03 +1100

    frontend structure done

--- client/src/App/components/tabledisplay/Table.js
+++ client/src/App/components/tabledisplay/Table.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import './style.css';
+import { makeStyles } from '@material-ui/core/styles'
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: "5px",
+        textAlign: "center",
+    },
+}));
 const Table = () => {
+
     return (
         <div>
             This is the table
+            <table>
+                <tr>
+                    <th>Ship Type</th>
+                    <th>Weight</th>
+                    <th>Home Port</th>
+                    <th>Ship Name</th>
+                    <th>Class</th>
+                    <th>Icon</th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                    <td>Germany</td>
+                    <td>Germany</td>
+                    <td>Germany</td>
+                </tr>
+               
+            </table>
         </div>
     )
 }
